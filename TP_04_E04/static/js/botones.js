2222db73156fed63b94860752ec48ffc472a7b2a
Nicolás Butterfield <nicobutter@gmail.com>
2025-05-14 10:15:50 -0300

    se agrega ejercicio 4

---
+++ TP_04_E04/static/js/botones.js
@@ -0,0 +1,19 @@
+function presion1() {
+  alert("Lunes");
+}
+
+function presion2() {
+  alert("Martes");
+}
+
+function presion3() {
+  alert("Miércoles");
+}
+
+function presion4() {
+  alert("Jueves");
+}
+
+function presion5() {
+  alert("Viernes");
+}
